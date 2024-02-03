674c3d7f50d639bbd9434ad518472f96268f2db6
hughjfchen <hugh.jf.chen@gmail.com>
2024-02-03 16:30:28 +0800

    really fixed the sudo mkdir -p problem.

--- collect-java-dump/deploy-packer.nix
+++ collect-java-dump/deploy-packer.nix
@@ -185,7 +185,7 @@ let
         for dirToMk in "${env.runDir}" "${env.dataDir}"
         do
           if [ ! -d "$dirToMk" ]; then
-             sudo -- sh -c "UMASK=0022 mkdir -p \"$dirToMk\""
+             sudo -- sh -c "umask u=rwx,go=rx; mkdir -p \"$dirToMk\""
              sudo chown -R ${env.processUser}:${env.processUser} "$dirToMk"
           fi
         done
